3cf2aa0f6eb880424a36a2f6c8768265e91c1344
SwatiGiri <35044589+SwatiGiri@users.noreply.github.com>
2018-03-01 22:56:16 -0800

    Comments updated.

--- py-poll/main.py
+++ py-poll/main.py
@@ -1,29 +1,16 @@
 
-# coding: utf-8
-
-# In[31]:
-
-
 import pandas as pd
-
-
-# In[32]:
-
 
 data_file = "C:/Users/swati/Downloads/Python/03-Python/Homework/Instructions/PyPoll/raw_data/election_data_2.csv"
 
 df = pd.read_csv(data_file)
 
-
-# In[33]:
-
-
-print(df.head())
+# print(df.head())
 
 
-# In[34]:
 
 
+# Header
 print("Election Results")
 print("-------------------------")
 
@@ -33,10 +20,7 @@ total_votes = len(df)
 print("Total Votes:", total_votes)
 print("-------------------------")
 
-
-# In[35]:
-
-
+# Finding Total Candidates
 candidate_list = df["Candidate"].value_counts()
 winner_name = ""
 winner_votes = 0
